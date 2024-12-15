222f89a10efed52e8b1069d007e719b7ee22e9dd
Juliano C L Santos <foxports@hotmail.com>
2024-12-15 18:10:09 -0300

    s3 config

--- s3.tf
+++ s3.tf
@@ -6,32 +6,7 @@ resource "aws_s3_bucket" "terraform-bucket-jcls" {
   }
 }
 
-#Politica de acesso
-resource "aws_s3_bucket_policy" "terraform-bucket-jcls-policy" {
-  bucket = aws_s3_bucket.terraform-bucket-jcls.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "DenyPublicRead"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.terraform-bucket-jcls.bucket}/*"
-      },
-      {
-        Sid       = "DenyPublicWrite"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:PutObject"
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.terraform-bucket-jcls.bucket}/*"
-      }
-    ]
-  })
-}
-
-# Configuração de bloqueio publico
+# Bloqueio de acesso público ao bucket
 resource "aws_s3_bucket_public_access_block" "terraform-bucket-jcls-access-block" {
   bucket = aws_s3_bucket.terraform-bucket-jcls.id
 
@@ -41,7 +16,7 @@ resource "aws_s3_bucket_public_access_block" "terraform-bucket-jcls-access-block
   restrict_public_buckets = true
 }
 
-# Regras para expirar arquivos
+# Configuração de ciclo de vida do bucket
 resource "aws_s3_bucket_lifecycle_configuration" "terraform-bucket-jcls-lifecycle" {
   bucket = aws_s3_bucket.terraform-bucket-jcls.id
 
